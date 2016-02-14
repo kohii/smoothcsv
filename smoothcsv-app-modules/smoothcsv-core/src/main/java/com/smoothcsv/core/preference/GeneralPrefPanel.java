5e0b25a14363a26d8497b6095ef4b87d3a92e375
kohii <kohii.git@gmail.com>
2016-02-14 20:15:26 +0900

    Show message label when change language configuration

--- smoothcsv-app-modules/smoothcsv-core/src/main/java/com/smoothcsv/core/preference/GeneralPrefPanel.java
+++ smoothcsv-app-modules/smoothcsv-core/src/main/java/com/smoothcsv/core/preference/GeneralPrefPanel.java
@@ -16,6 +16,8 @@ package com.smoothcsv.core.preference;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.List;
 
 import javax.swing.JLabel;
@@ -25,6 +27,7 @@ import com.smoothcsv.core.util.CoreSettings;
 import com.smoothcsv.framework.SCApplication;
 import com.smoothcsv.framework.modular.ModuleManifest.Language;
 import com.smoothcsv.framework.preference.PrefSelectBox;
+import com.smoothcsv.framework.util.MessageBundles;
 
 /**
  * @author kohii
@@ -37,16 +40,16 @@ public class GeneralPrefPanel extends JPanel {
     setBorder(null);
     GridBagLayout gridBagLayout = new GridBagLayout();
     gridBagLayout.columnWidths = new int[] {0, 0, 0};
-    gridBagLayout.rowHeights = new int[] {0, 0, 0};
+    gridBagLayout.rowHeights = new int[] {0, 0, 0, 0};
     gridBagLayout.columnWeights = new double[] {0.0, 1.0, Double.MIN_VALUE};
-    gridBagLayout.rowWeights = new double[] {0.0, 0.0, Double.MIN_VALUE};
+    gridBagLayout.rowWeights = new double[] {0.0, 0.0, 0.0, Double.MIN_VALUE};
     setLayout(gridBagLayout);
 
     JLabel lblNewLabel = new JLabel("Language");
     GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
     gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
     gbc_lblNewLabel.gridwidth = 2;
-    gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+    gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
     gbc_lblNewLabel.gridx = 0;
     gbc_lblNewLabel.gridy = 0;
     add(lblNewLabel, gbc_lblNewLabel);
@@ -56,9 +59,25 @@ public class GeneralPrefPanel extends JPanel {
     PrefSelectBox<Language> comboBox =
         new PrefSelectBox<>(CoreSettings.getInstance(), CoreSettings.LANGUAGE, langs, "id", "name");
     GridBagConstraints gbc_comboBox = new GridBagConstraints();
+    gbc_comboBox.insets = new Insets(0, 0, 5, 0);
     gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
     gbc_comboBox.gridx = 1;
     gbc_comboBox.gridy = 1;
     add(comboBox, gbc_comboBox);
+
+    JLabel restartMsgLabel = new JLabel(MessageBundles.getString("ISCA0011"));
+    restartMsgLabel.setVisible(false);
+    GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+    gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
+    gbc_lblNewLabel_1.gridx = 1;
+    gbc_lblNewLabel_1.gridy = 2;
+    add(restartMsgLabel, gbc_lblNewLabel_1);
+
+    comboBox.addItemListener(new ItemListener() {
+      @Override
+      public void itemStateChanged(ItemEvent e) {
+        restartMsgLabel.setVisible(true);
+      }
+    });
   }
 }
